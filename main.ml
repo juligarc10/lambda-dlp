77042099d170c7a14c3ef2b78c65d14f4ce799a6
Julián García Pallas <j.gpallas@udc.es>
2023-05-03 17:20:46 +0200

    Change final of expression ; for ;;

--- main.ml
+++ main.ml
@@ -8,8 +8,8 @@ open Lexer;;
 
 let rec read_lines acc =
   let line = read_line () in
-  if String.length line > 0 && String.get line (String.length line - 1) = ';' then
-    List.rev (String.sub line 0 (String.length line - 1) :: acc)
+  if String.length line > 1 && String.sub line (String.length line - 2) 2 = ";;" then
+    List.rev (String.sub line 0 (String.length line - 2) :: acc)
   else
     read_lines (line :: acc)
 
